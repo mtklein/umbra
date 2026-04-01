1c5122f2933ecdf87ed1632b1c9b472b92ba9ca9
Mike Klein <mtklein@gmail.com>
2026-04-01 14:46:51 -0500

    tidy

--- include/umbra_uniforms.h
+++ include/umbra_uniforms.h
@@ -1,19 +1,20 @@
 #pragma once
 #include <stddef.h>
 
+struct umbra_uniforms* umbra_uniforms(void);
+void   umbra_uniforms_free(struct umbra_uniforms*);
+
 typedef struct { size_t off; } umbra_uniform;
 
-struct umbra_uniforms* umbra_uniforms     (void);
-void                   umbra_uniforms_free(struct umbra_uniforms*);
-
-umbra_uniform     umbra_reserve_f32     (struct umbra_uniforms*, int n);
+umbra_uniform umbra_reserve_f32(struct umbra_uniforms*, int n);
 umbra_uniform umbra_reserve_ptr(struct umbra_uniforms*);
-size_t            umbra_uniforms_size    (struct umbra_uniforms const*);
-void              umbra_uniforms_set_size(struct umbra_uniforms*, size_t);
 
-void      umbra_set_f32(struct umbra_uniforms*, umbra_uniform,     float const*, int n);
-void      umbra_set_ptr(struct umbra_uniforms*, umbra_uniform,
-                        void *ptr, size_t sz, _Bool read_only, size_t row_bytes);
+size_t umbra_uniforms_size    (struct umbra_uniforms const*);
+void   umbra_uniforms_set_size(struct umbra_uniforms*, size_t);
+
+void umbra_set_f32(struct umbra_uniforms*, umbra_uniform,
+                   float const*, int n);
+void umbra_set_ptr(struct umbra_uniforms*, umbra_uniform,
+                   void *ptr, size_t sz, _Bool read_only, size_t row_bytes);
 
 void* umbra_uniforms_data(struct umbra_uniforms const*);
-
