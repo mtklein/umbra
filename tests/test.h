2a37422e0e3423264a6a4acf96505d2790b7f903
Mike Klein <mtklein@gmail.com>
2026-04-09 08:16:15 -0500

    tests: move test.h helper definitions into test_main.c
    equiv/val_eq/val_lt and the test_backends_make/run/free trio were
    static inline in the header. Declare them in test.h, define them in
    test_main.c (the test harness file that already lives next to the
    helpers).

--- tests/test.h
+++ tests/test.h
@@ -19,13 +19,9 @@ void test_register(test_fn fn);
 
 #define here ? (void)0 : (dprintf(2, "%s:%d failed\n", __FILE__, __LINE__), __builtin_trap())
 
-static inline _Bool equiv(float x, float y) {
-    return (x <= y && y <= x) || (x != x && y != y);
-}
-static inline _Bool val_eq(umbra_val32 a, umbra_val32 b) { return a.id == b.id && a.chan == b.chan; }
-static inline _Bool val_lt(umbra_val32 a, umbra_val32 b) {
-    return a.id < b.id || (a.id == b.id && a.chan < b.chan);
-}
+_Bool equiv (float x, float y);
+_Bool val_eq(umbra_val32 a, umbra_val32 b);
+_Bool val_lt(umbra_val32 a, umbra_val32 b);
 
 enum { NUM_BACKENDS = 4 };
 
@@ -34,36 +30,7 @@ struct test_backends {
     struct umbra_program *p[NUM_BACKENDS];
 };
 
-static inline struct test_backends test_backends_make(struct umbra_basic_block const *bb) {
-    struct test_backends B;
-    B.be[0] = umbra_backend_interp();
-    B.be[1] = umbra_backend_jit();
-    B.be[2] = umbra_backend_metal();
-    B.be[3] = umbra_backend_vulkan();
-    for (int i = 0; i < NUM_BACKENDS; i++) {
-        B.p[i] = B.be[i] ? B.be[i]->compile(B.be[i], bb) : NULL;
-    }
-    B.p[0] != 0 here;
-#if defined(__aarch64__) || defined(__AVX2__)
-    B.p[1] != 0 here;
-#endif
-#if (defined(__APPLE__) && defined(__clang__)) && !defined(__wasm__)
-    B.p[2] != 0 here;
-#endif
-    return B;
-}
-
-static inline _Bool test_backends_run(struct test_backends *B, int bi, int r, int b,
-                                      struct umbra_buf buf[]) {
-    if (!B->p[bi]) { return 0; }
-    B->p[bi]->queue(B->p[bi], 0, 0, r, b, buf);
-    B->be[bi]->flush(B->be[bi]);
-    return 1;
-}
-
-static inline void test_backends_free(struct test_backends *B) {
-    for (int i = 0; i < NUM_BACKENDS; i++) {
-        if (B->p[i]) { B->p[i]->free(B->p[i]); }
-        if (B->be[i]) { B->be[i]->free(B->be[i]); }
-    }
-}
+struct test_backends test_backends_make(struct umbra_basic_block const *bb);
+_Bool                test_backends_run (struct test_backends *B, int bi, int r, int b,
+                                        struct umbra_buf buf[]);
+void                 test_backends_free(struct test_backends *B);
--- tests/test_main.c
+++ tests/test_main.c
@@ -11,6 +11,48 @@ void test_register(test_fn fn) {
     }
 }
 
+_Bool equiv(float x, float y) {
+    return (x <= y && y <= x) || (x != x && y != y);
+}
+_Bool val_eq(umbra_val32 a, umbra_val32 b) { return a.id == b.id && a.chan == b.chan; }
+_Bool val_lt(umbra_val32 a, umbra_val32 b) {
+    return a.id < b.id || (a.id == b.id && a.chan < b.chan);
+}
+
+struct test_backends test_backends_make(struct umbra_basic_block const *bb) {
+    struct test_backends B;
+    B.be[0] = umbra_backend_interp();
+    B.be[1] = umbra_backend_jit();
+    B.be[2] = umbra_backend_metal();
+    B.be[3] = umbra_backend_vulkan();
+    for (int i = 0; i < NUM_BACKENDS; i++) {
+        B.p[i] = B.be[i] ? B.be[i]->compile(B.be[i], bb) : NULL;
+    }
+    B.p[0] != 0 here;
+#if defined(__aarch64__) || defined(__AVX2__)
+    B.p[1] != 0 here;
+#endif
+#if (defined(__APPLE__) && defined(__clang__)) && !defined(__wasm__)
+    B.p[2] != 0 here;
+#endif
+    return B;
+}
+
+_Bool test_backends_run(struct test_backends *B, int bi, int r, int b,
+                        struct umbra_buf buf[]) {
+    if (!B->p[bi]) { return 0; }
+    B->p[bi]->queue(B->p[bi], 0, 0, r, b, buf);
+    B->be[bi]->flush(B->be[bi]);
+    return 1;
+}
+
+void test_backends_free(struct test_backends *B) {
+    for (int i = 0; i < NUM_BACKENDS; i++) {
+        if (B->p[i]) { B->p[i]->free(B->p[i]); }
+        if (B->be[i]) { B->be[i]->free(B->be[i]); }
+    }
+}
+
 int main(void) {
     for (int i = 0; i < test_count; i++) {
         registry[i]();
