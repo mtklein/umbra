271c498322eb0473c61cf26e120b4b4812247ce3
Mike Klein <mtklein@gmail.com>
2026-04-14 15:58:12 -0500

    todo

--- src/fingerprint.h
+++ src/fingerprint.h
@@ -4,5 +4,6 @@
 
 typedef struct { uint64_t lo, hi; } fingerprint;
 
+// TODO: is there a way to reduce this API as used into a single compute-and-compare?
 fingerprint fingerprint_hash(void const *buf, size_t bytes);
 _Bool fingerprint_eq(fingerprint, fingerprint);
