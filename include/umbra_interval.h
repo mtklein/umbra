4b827a24a4dfa47a631ca7ed373a00c9895d0dae
Mike Klein <mtklein@gmail.com>
2026-04-16 14:44:23 -0500

    fmt

--- include/umbra_interval.h
+++ include/umbra_interval.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "umbra.h"
 
-typedef struct { umbra_val32 lo, hi; } umbra_interval;
+typedef struct {
+    umbra_val32 lo, hi;
+} umbra_interval;
 
-umbra_interval umbra_interval_exact    (struct umbra_builder*, float v);
-umbra_interval umbra_interval_uniform  (struct umbra_builder*, umbra_ptr32, int slot);
-umbra_interval umbra_interval_add_f32  (struct umbra_builder*, umbra_interval, umbra_interval);
-umbra_interval umbra_interval_sub_f32  (struct umbra_builder*, umbra_interval, umbra_interval);
-umbra_interval umbra_interval_mul_f32  (struct umbra_builder*, umbra_interval, umbra_interval);
-umbra_interval umbra_interval_div_f32  (struct umbra_builder*, umbra_interval, umbra_interval);
-umbra_interval umbra_interval_min_f32  (struct umbra_builder*, umbra_interval, umbra_interval);
-umbra_interval umbra_interval_max_f32  (struct umbra_builder*, umbra_interval, umbra_interval);
-umbra_interval umbra_interval_sqrt_f32 (struct umbra_builder*, umbra_interval);
-umbra_interval umbra_interval_abs_f32  (struct umbra_builder*, umbra_interval);
+umbra_interval umbra_interval_exact   (struct umbra_builder*, float v);
+umbra_interval umbra_interval_uniform (struct umbra_builder*, umbra_ptr32, int slot);
+umbra_interval umbra_interval_add_f32 (struct umbra_builder*, umbra_interval, umbra_interval);
+umbra_interval umbra_interval_sub_f32 (struct umbra_builder*, umbra_interval, umbra_interval);
+umbra_interval umbra_interval_mul_f32 (struct umbra_builder*, umbra_interval, umbra_interval);
+umbra_interval umbra_interval_div_f32 (struct umbra_builder*, umbra_interval, umbra_interval);
+umbra_interval umbra_interval_min_f32 (struct umbra_builder*, umbra_interval, umbra_interval);
+umbra_interval umbra_interval_max_f32 (struct umbra_builder*, umbra_interval, umbra_interval);
+umbra_interval umbra_interval_sqrt_f32(struct umbra_builder*, umbra_interval);
+umbra_interval umbra_interval_abs_f32 (struct umbra_builder*, umbra_interval);
